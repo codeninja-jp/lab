ca8c60f5344e3434f94b38380f6290b98fb5a10e
codeninja-jp <mr.ninja.dev@gmail.com>
2023-04-21 10:19:52 +0900

    Add Java

---
+++ aws/cli/s3/s3_cli.sh
@@ -0,0 +1,6 @@
+# Fetch list of buckets
+aws s3 ls
+
+#Fetch objects in a s3 Bucket
+aws s3 ls ${bucket_name}
+
---
+++ demo/src/main/java/com/example/utils/JSONFormatter.java
@@ -0,0 +1,36 @@
+package com.example.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import org.json.JSONObject;
+
+public class JSONFormatter {
+    private static final Logger LOG = Logger.getLogger(JSONFormatter.class.getName());
+
+
+    private static String loadFile(String path) throws IOException {
+        LOG.info("Loading JSON file...");
+        String content = new String(Files.readAllBytes(Paths.get(path)));
+        LOG.info("Loaded JSON file: " + content);
+        return content;
+    }
+
+    private static JSONObject put(String strJson, String key, String newValue) throws IOException {
+        LOG.info("Converting input to JSONObject: " + strJson);
+        JSONObject json = new JSONObject(strJson);
+        LOG.info("Converted input to JSONObject: " + json);
+        LOG.info("Replacing value: " + newValue);
+        json.put(key, newValue);
+        LOG.info("Updated value: " + json.toString());
+        return json;
+    }
+
+    public static String format(String path, String key, String newValue) throws IOException {
+        String json = loadFile(path);
+        JSONObject result = put(json, key, newValue);
+        return result.toString();
+    }
+}
